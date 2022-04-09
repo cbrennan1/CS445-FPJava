95d4604e3e9b1cafea5fa839bd87eb746270a31d
Colin Brennan <cbrennan1@hawk.iit.edu>
2022-04-09 12:58:17 -0500

    java eclipse project

---
+++ javaCode/src/main/java/Accounts.java
@@ -0,0 +1,14 @@
+package main.java;
+
+public class Accounts {
+
+	public Accounts() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
