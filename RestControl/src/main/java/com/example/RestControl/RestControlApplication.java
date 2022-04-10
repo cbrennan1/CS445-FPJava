bd4092be756b1d2897e539684d1ea54b3e50c3f7
Colin Brennan <cbrennan1@hawk.iit.edu>
2022-04-09 22:59:02 -0500

    website up

--- RestControl/src/main/java/com/example/RestControl/RestControlApplication.java
+++ RestControl/src/main/java/com/example/RestControl/RestControlApplication.java
@@ -1,6 +1,8 @@
 package com.example.RestControl;
 // Test
 
+import java.util.concurrent.atomic.AtomicLong;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,9 +23,12 @@ public class RestControlApplication {
 public String hello(@RequestParam(value = "name", defaultValue = "Computers") String name) {
 return String.format("Hello %s!", name); }
 
+private static final String template = "Hello";
+private final AtomicLong counter = new AtomicLong();
 
-@GetMapping("/bn/api/accounts")
-public String display(@RequestParam(value = "name", defaultValue = "World") String name) {
-return String.format("Hello %s!", name); }
+    @GetMapping("/bn/api/accounts")
+    public accounts accounts(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new accounts(counter.incrementAndGet(), String.format(template,name), name, name, 0, name, name, false, name);
+    }
 
 }
