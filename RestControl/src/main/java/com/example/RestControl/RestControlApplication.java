f3eb54c570d6b480f648df95fbb842e96df34adc
Colin Brennan <cbrennan1@hawk.iit.edu>
2022-04-09 19:18:51 -0500

    rest control

---
+++ RestControl/src/main/java/com/example/RestControl/RestControlApplication.java
@@ -0,0 +1,13 @@
+package com.example.RestControl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestControlApplication.class, args);
+	}
+
+}
---
+++ RestControl/src/test/java/com/example/RestControl/RestControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RestControl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
