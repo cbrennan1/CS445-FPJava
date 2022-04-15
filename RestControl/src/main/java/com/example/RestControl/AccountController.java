75255fc7973c6c1cf7484e789360ad8b0da441de
Colin Brennan <cbrennan1@hawk.iit.edu>
2022-04-15 16:09:31 -0500

    new Date();

--- RestControl/src/main/java/com/example/RestControl/AccountController.java
+++ RestControl/src/main/java/com/example/RestControl/AccountController.java
@@ -20,8 +20,8 @@ public class AccountController {
 
     @GetMapping("/bn/api/accounts")
     public List<accounts> getAccounts () {
-        Calendar calendar = Calendar.getInstance();
-        Date date_created = calendar.getTime();
+        //Calendar calendar = Calendar.getInstance();
+        Date date_created = new Date();
         accounts a0 = new accounts("<uid0>", "Virgil Bistriceanu", "10 West", "31st ST", 60616, "312-567-5146", "http://cs.iit.edu/~virgil/pictures/virgil-head-small-200811.jpg", true, date_created);
         accounts a1 = new accounts("<uid1", "Jane Smith", "123 ", "2nd ST", 60607, "217-456-7890", "http://example.com/images/jane-smith.jpeg", false, date_created);
         accounts a2 = new accounts("<uid2>", "CSR #1", "101 West", "Main ST", 60010, "(847) 842-8048", "http://example.com/images/jane-smith.jpeg", true, date_created);
