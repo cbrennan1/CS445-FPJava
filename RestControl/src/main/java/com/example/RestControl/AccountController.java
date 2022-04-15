f7e500700b223f0a4bc1ed3d75a062acde65f5f2
Colin Brennan <cbrennan1@hawk.iit.edu>
2022-04-15 16:16:36 -0500

    formatting

--- RestControl/src/main/java/com/example/RestControl/AccountController.java
+++ RestControl/src/main/java/com/example/RestControl/AccountController.java
@@ -19,16 +19,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class AccountController {
     private static final String template = "Hello";
     private final AtomicLong counter = new AtomicLong();
-    private final Date = date_created;
 
     @GetMapping("/bn/api/accounts")
     public List<accounts> getAccounts () {
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
-        LocalDateTime now = LocalDateTime.now(); 
-        date_created = (dtf.format(now)); 
-        accounts a0 = new accounts("<uid0>", "Virgil Bistriceanu", "10 West", "31st ST", 60616, "312-567-5146", "http://cs.iit.edu/~virgil/pictures/virgil-head-small-200811.jpg", true, date_created);
-        accounts a1 = new accounts("<uid1", "Jane Smith", "123 ", "2nd ST", 60607, "217-456-7890", "http://example.com/images/jane-smith.jpeg", false, date_created);
-        accounts a2 = new accounts("<uid2>", "CSR #1", "101 West", "Main ST", 60010, "(847) 842-8048", "http://example.com/images/jane-smith.jpeg", true, date_created);
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");  
+        Date date_created = new Date();   
+        accounts a0 = new accounts("<uid0>", "Virgil Bistriceanu", "10 West", "31st ST", 60616, "312-567-5146", "http://cs.iit.edu/~virgil/pictures/virgil-head-small-200811.jpg", true, (formatter.format(date)));
+        accounts a1 = new accounts("<uid1", "Jane Smith", "123 ", "2nd ST", 60607, "217-456-7890", "http://example.com/images/jane-smith.jpeg", false, (formatter.format(date)));
+        accounts a2 = new accounts("<uid2>", "CSR #1", "101 West", "Main ST", 60010, "(847) 842-8048", "http://example.com/images/jane-smith.jpeg", true, (formatter.format(date)));
         List<accounts> list = new ArrayList<>();
         list.add(a0);
         list.add(a1);
