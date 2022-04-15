e901a19bfa7f4381997a720cf94090bb862ea47f
Colin Brennan <cbrennan1@hawk.iit.edu>
2022-04-15 17:06:05 -0500

    set counter for uid to 0

--- RestControl/src/main/java/com/example/RestControl/AccountController.java
+++ RestControl/src/main/java/com/example/RestControl/AccountController.java
@@ -20,14 +20,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AccountController {
     private static final String template = "Hello";
-    private final AtomicLong counter = new AtomicLong();
+    private final AtomicLong counter = new AtomicLong(0);
 
     @GetMapping("/bn/api/accounts")
     public List<accounts> getAccounts () {
         DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("MM-dd-yyyy HH:mm:ss");
         LocalDateTime date_created = LocalDateTime.now();
         String formatted_date = date_created.format(myFormatObj);
-        accounts a0 = new accounts("<uid" + counter.incrementAndGet() - 1 + ">", "Virgil Bistriceanu", "10 West", "31st ST", 60616, "312-567-5146", "http://cs.iit.edu/~virgil/pictures/virgil-head-small-200811.jpg", true, formatted_date);
+        accounts a0 = new accounts("<uid" + counter.incrementAndGet() + ">", "Virgil Bistriceanu", "10 West", "31st ST", 60616, "312-567-5146", "http://cs.iit.edu/~virgil/pictures/virgil-head-small-200811.jpg", true, formatted_date);
         accounts a1 = new accounts("<uid1", "Jane Smith", "123 ", "2nd ST", 60607, "217-456-7890", "http://example.com/images/jane-smith.jpeg", false, formatted_date);
         accounts a2 = new accounts("<uid2>", "CSR #1", "101 West", "Main ST", 60010, "(847) 842-8048", "http://example.com/images/jane-smith.jpeg", true, formatted_date);
         List<accounts> list = new ArrayList<>();
