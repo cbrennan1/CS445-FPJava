c2fed4d3fb798b1971e601f003d9abe95e6cfdcc
Colin Brennan <cbrennan1@hawk.iit.edu>
2022-04-15 18:18:21 -0500

    semi colons

--- RestControl/src/main/java/com/example/RestControl/AccountController.java
+++ RestControl/src/main/java/com/example/RestControl/AccountController.java
@@ -27,9 +27,9 @@ public class AccountController {
         DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("MM-dd-yyyy HH:mm:ss");
         LocalDateTime date_created = LocalDateTime.now();
         String formatted_date = date_created.format(myFormatObj);
-        address addy0 = new address("10 West 31st ST", 60616)
-        address addy1 = new address("123 2nd ST", 60607)
-        address addy2 = new address("101 West Main St.", 60010)
+        address addy0 = new address("10 West 31st ST", 60616);
+        address addy1 = new address("123 2nd ST", 60607);
+        address addy2 = new address("101 West Main St.", 60010);
         accounts a0 = new accounts("<uid" + counter.getAndIncrement() + ">", "Virgil Bistriceanu", addy0, "312-567-5146", "http://cs.iit.edu/~virgil/pictures/virgil-head-small-200811.jpg", true, formatted_date);
         accounts a1 = new accounts("<uid" + counter.getAndIncrement() + ">", "Jane Smith", addy1, "217-456-7890", "http://example.com/images/jane-smith.jpeg", false, formatted_date);
         accounts a2 = new accounts("<uid" + counter.getAndIncrement() + ">", "CSR #1", addy2, "(847) 842-8048", "http://example.com/images/jane-smith.jpeg", true, formatted_date);
