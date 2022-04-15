dbb9cb73dfc7512e39717882229fdb71de5e5504
Colin Brennan <cbrennan1@hawk.iit.edu>
2022-04-15 17:08:09 -0500

    counter get and increment for all three uid

--- RestControl/src/main/java/com/example/RestControl/AccountController.java
+++ RestControl/src/main/java/com/example/RestControl/AccountController.java
@@ -27,9 +27,9 @@ public class AccountController {
         DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("MM-dd-yyyy HH:mm:ss");
         LocalDateTime date_created = LocalDateTime.now();
         String formatted_date = date_created.format(myFormatObj);
-        accounts a0 = new accounts("<uid" + counter.incrementAndGet() + ">", "Virgil Bistriceanu", "10 West", "31st ST", 60616, "312-567-5146", "http://cs.iit.edu/~virgil/pictures/virgil-head-small-200811.jpg", true, formatted_date);
-        accounts a1 = new accounts("<uid1", "Jane Smith", "123 ", "2nd ST", 60607, "217-456-7890", "http://example.com/images/jane-smith.jpeg", false, formatted_date);
-        accounts a2 = new accounts("<uid2>", "CSR #1", "101 West", "Main ST", 60010, "(847) 842-8048", "http://example.com/images/jane-smith.jpeg", true, formatted_date);
+        accounts a0 = new accounts("<uid" + counter.getAndIncrement() + ">", "Virgil Bistriceanu", "10 West", "31st ST", 60616, "312-567-5146", "http://cs.iit.edu/~virgil/pictures/virgil-head-small-200811.jpg", true, formatted_date);
+        accounts a1 = new accounts("<uid" + counter.getAndIncrement() + ">", "Jane Smith", "123 ", "2nd ST", 60607, "217-456-7890", "http://example.com/images/jane-smith.jpeg", false, formatted_date);
+        accounts a2 = new accounts("<uid" + counter.getAndIncrement() + ">", "CSR #1", "101 West", "Main ST", 60010, "(847) 842-8048", "http://example.com/images/jane-smith.jpeg", true, formatted_date);
         List<accounts> list = new ArrayList<>();
         list.add(a0);
         list.add(a1);
