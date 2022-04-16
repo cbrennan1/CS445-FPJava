e6c83f30adafdcf71782ddbf22eb516a7327aaae
Colin Brennan <cbrennan1@hawk.iit.edu>
2022-04-15 20:41:52 -0500

    imports

--- RestControl/src/main/java/com/example/RestControl/AccountController.java
+++ RestControl/src/main/java/com/example/RestControl/AccountController.java
@@ -11,7 +11,8 @@ import java.time.format.DateTimeFormatter;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
