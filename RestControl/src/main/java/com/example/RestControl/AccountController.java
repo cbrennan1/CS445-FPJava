6205490e66945fbd7563f8d45eee2dbcd4e581e8
Colin Brennan <cbrennan1@hawk.iit.edu>
2022-04-19 18:14:25 -0500

    testing

--- RestControl/src/main/java/com/example/RestControl/AccountController.java
+++ RestControl/src/main/java/com/example/RestControl/AccountController.java
@@ -19,28 +19,25 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-public class AccountController {
-    private static final String template = "Hello";
-    private final AtomicLong counter = new AtomicLong(0);
-
+public class AccountsController {
+	private final AtomicLong counter = new AtomicLong();
     DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("MM-dd-yyyy HH:mm:ss");
     LocalDateTime date_created = LocalDateTime.now();
     String formatted_date = date_created.format(myFormatObj);
-
-
-	accounts account0 = new accounts("<uid" + counter.getAndIncrement() + ">", "Virgil Bistriceanu", "West 31st ST", "60616", "312-567-5146", "http://cs.iit.edu/~virgil/pictures/virgil-head-small-200811.jpg", true, formatted_date);
-	accounts account1 = new accounts("<uid" + counter.getAndIncrement() + ">", "Jane Smith", "2nd ST", "60607", "217-456-7890", "http://example.com/images/jane-smith.jpeg", false, formatted_date);
-	accounts account2 = new accounts("<uid" + counter.getAndIncrement() + ">", "CSR #1",  "West Main St.", "60616", "(847) 842-8048", "http://example.com/images/jane-smith.jpeg", true, formatted_date);
+    
+	Accounts account0 = new Accounts("<uid" + counter.getAndIncrement() + ">", "Virgil Bistriceanu", "West 31st ST", "60616", "312-567-5146", "http://cs.iit.edu/~virgil/pictures/virgil-head-small-200811.jpg", true, formatted_date);
+	Accounts account1 = new Accounts("<uid" + counter.getAndIncrement() + ">", "Jane Smith", "2nd ST", "60607", "217-456-7890", "http://example.com/images/jane-smith.jpeg", false, formatted_date);
+	Accounts account2 = new Accounts("<uid" + counter.getAndIncrement() + ">", "CSR #1",  "West Main St.", "60616", "(847) 842-8048", "http://example.com/images/jane-smith.jpeg", true, formatted_date);
     	
     
 	@RequestMapping(value = "/bn/api/accounts", method = RequestMethod.GET)
-	public List<accounts> getAccounts() {
-	    List<accounts> list = new ArrayList<>();
+	public List<Accounts> getAccounts() {
+	    List<Accounts> list = new ArrayList<>();
 	    list.add(account0);
 	    list.add(account1);
 	    list.add(account2);
 	    return list;
 	}
 
-    
+
 }
