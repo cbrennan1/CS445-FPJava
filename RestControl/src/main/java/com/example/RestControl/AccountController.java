779c57ffe6a27af43fc3480ba2ea714dee72348b
Colin Brennan <cbrennan1@hawk.iit.edu>
2022-04-19 17:10:16 -0500

    fixed controller

--- RestControl/src/main/java/com/example/RestControl/AccountController.java
+++ RestControl/src/main/java/com/example/RestControl/AccountController.java
@@ -27,18 +27,19 @@ public class AccountController {
     LocalDateTime date_created = LocalDateTime.now();
     String formatted_date = date_created.format(myFormatObj);
 
-    accounts a0 = new accounts("<uid" + counter.getAndIncrement() + ">", "Virgil Bistriceanu", "10",  "West 31st ST", "60616", "312-567-5146", "http://cs.iit.edu/~virgil/pictures/virgil-head-small-200811.jpg", true, formatted_date);
-    accounts a1 = new accounts("<uid" + counter.getAndIncrement() + ">", "Jane Smith", "123",  "2nd ST", "60607", "217-456-7890", "http://example.com/images/jane-smith.jpeg", false, formatted_date);
-    accounts a2 = new accounts("<uid" + counter.getAndIncrement() + ">", "CSR #1",  "01",  "West Main St.", "60616", "(847) 842-8048", "http://example.com/images/jane-smith.jpeg", true, formatted_date);
-       
-    @RequestMapping(value = "/bn/api/accounts", method = RequestMethod.GET)
-    public List<accounts> getAccounts() {
-        List<accounts> list = new ArrayList<>();
-        list.add(a0);
-        list.add(a1);
-        list.add(a2);
-        return list;
-    }
+	accounts account0 = new accounts("<uid" + counter.getAndIncrement() + ">", "Virgil Bistriceanu", "West 31st ST", "60616", "312-567-5146", "http://cs.iit.edu/~virgil/pictures/virgil-head-small-200811.jpg", true, formatted_date);
+	accounts account1 = new accounts("<uid" + counter.getAndIncrement() + ">", "Jane Smith", "2nd ST", "60607", "217-456-7890", "http://example.com/images/jane-smith.jpeg", false, formatted_date);
+	accounts account2 = new accounts("<uid" + counter.getAndIncrement() + ">", "CSR #1",  "West Main St.", "60616", "(847) 842-8048", "http://example.com/images/jane-smith.jpeg", true, formatted_date);
+    	
+    
+	@RequestMapping(value = "/bn/api/accounts", method = RequestMethod.GET)
+	public List<Accounts> getAccounts() {
+	    List<Accounts> list = new ArrayList<>();
+	    list.add(account0);
+	    list.add(account1);
+	    list.add(account2);
+	    return list;
+	}
 
     
 }
