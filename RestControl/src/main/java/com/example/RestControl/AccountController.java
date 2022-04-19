c7d734c05719fdac27e951fe4b525148c1ac47ca
Colin Brennan <cbrennan1@hawk.iit.edu>
2022-04-19 18:15:22 -0500

    fixed capitals

--- RestControl/src/main/java/com/example/RestControl/AccountController.java
+++ RestControl/src/main/java/com/example/RestControl/AccountController.java
@@ -25,14 +25,14 @@ public class AccountsController {
     LocalDateTime date_created = LocalDateTime.now();
     String formatted_date = date_created.format(myFormatObj);
     
-	Accounts account0 = new Accounts("<uid" + counter.getAndIncrement() + ">", "Virgil Bistriceanu", "West 31st ST", "60616", "312-567-5146", "http://cs.iit.edu/~virgil/pictures/virgil-head-small-200811.jpg", true, formatted_date);
-	Accounts account1 = new Accounts("<uid" + counter.getAndIncrement() + ">", "Jane Smith", "2nd ST", "60607", "217-456-7890", "http://example.com/images/jane-smith.jpeg", false, formatted_date);
-	Accounts account2 = new Accounts("<uid" + counter.getAndIncrement() + ">", "CSR #1",  "West Main St.", "60616", "(847) 842-8048", "http://example.com/images/jane-smith.jpeg", true, formatted_date);
+	accounts account0 = new accounts("<uid" + counter.getAndIncrement() + ">", "Virgil Bistriceanu", "West 31st ST", "60616", "312-567-5146", "http://cs.iit.edu/~virgil/pictures/virgil-head-small-200811.jpg", true, formatted_date);
+	accounts account1 = new accounts("<uid" + counter.getAndIncrement() + ">", "Jane Smith", "2nd ST", "60607", "217-456-7890", "http://example.com/images/jane-smith.jpeg", false, formatted_date);
+	accounts account2 = new accounts("<uid" + counter.getAndIncrement() + ">", "CSR #1",  "West Main St.", "60616", "(847) 842-8048", "http://example.com/images/jane-smith.jpeg", true, formatted_date);
     	
     
 	@RequestMapping(value = "/bn/api/accounts", method = RequestMethod.GET)
-	public List<Accounts> getAccounts() {
-	    List<Accounts> list = new ArrayList<>();
+	public List<accounts> getAccounts() {
+	    List<accounts> list = new ArrayList<>();
 	    list.add(account0);
 	    list.add(account1);
 	    list.add(account2);
