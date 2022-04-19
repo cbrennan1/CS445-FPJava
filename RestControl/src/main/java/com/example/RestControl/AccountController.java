96ec62ac342e50d5fa30ab4fdbee758c03b8c22c
Colin Brennan <cbrennan1@hawk.iit.edu>
2022-04-19 17:12:56 -0500

    fixed capitilization

--- RestControl/src/main/java/com/example/RestControl/AccountController.java
+++ RestControl/src/main/java/com/example/RestControl/AccountController.java
@@ -33,8 +33,8 @@ public class AccountController {
     	
     
 	@RequestMapping(value = "/bn/api/accounts", method = RequestMethod.GET)
-	public List<Accounts> getAccounts() {
-	    List<Accounts> list = new ArrayList<>();
+	public List<accounts> getAccounts() {
+	    List<accounts> list = new ArrayList<>();
 	    list.add(account0);
 	    list.add(account1);
 	    list.add(account2);
