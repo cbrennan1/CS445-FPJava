d7decdf42216ca2172a626e9bde185a702aea837
Colin Brennan <cbrennan1@hawk.iit.edu>
2022-04-20 16:07:49 -0500

    testing

--- RestControl/src/main/java/com/example/RestControl/AccountsService.java
+++ RestControl/src/main/java/com/example/RestControl/AccountsService.java
@@ -58,14 +58,14 @@ public class AccountsService {
 	}
 
 	public Accounts addAccount(Accounts newAccount) {
-		Accounts account3 = new Accounts("<uid" + counter.getAndIncrement() + ">", newAccount.getName(),  newAccount.getAddress(), newAccount.getPhone(), newAccount.getPicture(), newAccount.getIs_active(), newAccount.getDate_Created());
+		account3 = new Accounts(account3.getUid(), newAccount.getName(),  newAccount.getAddress(), newAccount.getPhone(), newAccount.getPicture(), newAccount.getIs_active(), newAccount.getDate_Created());
 		List<Accounts> list = getAccounts();
 		list.add(account3);
 		return account3;
 	}
 	
 	public Accounts putAccount(Accounts newAccount) {
-		account3 = new Accounts(account3.getUid(), newAccount.getName(),  newAccount.getAddress(), newAccount.getPhone(), newAccount.getPicture(), newAccount.getIs_active(), newAccount.getDate_Created());
+		account3 = new Accounts(newAccount.getUid(), newAccount.getName(),  newAccount.getAddress(), newAccount.getPhone(), newAccount.getPicture(), newAccount.getIs_active(), newAccount.getDate_Created());
 		return null;
 	}
 	/*
