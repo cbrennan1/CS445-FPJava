6017cfd5bd86623bd78c31940dde8b3a6f16f1e2
Colin Brennan <cbrennan1@hawk.iit.edu>
2022-04-20 14:25:31 -0500

    new put test

--- RestControl/src/main/java/com/example/RestControl/AccountRestController.java
+++ RestControl/src/main/java/com/example/RestControl/AccountRestController.java
@@ -95,10 +95,19 @@ public class AccountRestController {
 	}
 	
 	@PutMapping("/accounts/{uid3}")
-	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public ResponseEntity<Accounts> put(@PathVariable("uid") String accountUid) {
+			
+			Accounts account = accountsService.getAccountsbyUID(accountUid);
+			if(account == null) {
+				return new ResponseEntity<Accounts> (account,HttpStatus.NOT_FOUND);
+			}
+			return new ResponseEntity<Accounts> (account,HttpStatus.OK);
+		}
+	/*
 	public Accounts putAccount(@RequestBody Accounts newAccount, HttpServletResponse response) {
         return null;
 	}
+	*/
 	
 	@GetMapping("/accounts/{uid3}")
 	@ResponseStatus(HttpStatus.OK)
