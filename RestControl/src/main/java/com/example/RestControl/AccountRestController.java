2a5ec0327a54d3f4abfbfb3715569bed15ec874d
Colin Brennan <cbrennan1@hawk.iit.edu>
2022-04-19 22:41:31 -0500

    httpstatus created

--- RestControl/src/main/java/com/example/RestControl/AccountRestController.java
+++ RestControl/src/main/java/com/example/RestControl/AccountRestController.java
@@ -21,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -83,7 +84,9 @@ public class AccountRestController {
 	}
 	
 	@PostMapping("/accounts")
+	@ResponseStatus(HttpStatus.CREATED)
 	public Accounts addAccount(@RequestBody Accounts newAccount) {
+
 		return accountsService.addAccount(newAccount);
 	}
 
