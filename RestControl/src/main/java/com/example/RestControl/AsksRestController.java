d84d83d559d38214a535daecbb0a5f2251eae890
Colin Brennan <cbrennan1@hawk.iit.edu>
2022-04-21 11:58:37 -0500

    adding get/put for asks testing

--- RestControl/src/main/java/com/example/RestControl/AsksRestController.java
+++ RestControl/src/main/java/com/example/RestControl/AsksRestController.java
@@ -5,6 +5,7 @@ import java.util.concurrent.atomic.AtomicLong;
 import javax.servlet.http.HttpServletResponse;
 
 import java.util.List;
+import java.util.Map;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Calendar;
@@ -14,8 +15,11 @@ import java.time.format.DateTimeFormatter;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -33,7 +37,22 @@ public class AsksRestController {
 	@PostMapping("/bn/api/accounts/{uid}/asks")
 	@ResponseStatus(HttpStatus.CREATED)
 	public Asks addAsk(@RequestBody Asks newAsks, HttpServletResponse response) {
-		response.setHeader("Location", "/bn/api/accounts/{uid}/asks/{uid}" + newAsks.getUid());
+		response.setHeader("Location", "/bn/api/accounts/{uid}/asks" + newAsks.getUid());
 		return asksService.addAsks(newAsks);
+	}
+	@GetMapping("/bn/api/asks/{aid}")
+	public ResponseEntity<Asks> getAsksByAid(@PathVariable("aid")String askId) {
+		Asks asks = asksService.getAsksByAid(askId);
+		if(asks == null) {
+			return new ResponseEntity<Asks> (HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<Asks> (asks,HttpStatus.OK);
+	}
+	@PutMapping("/bn/api/accounts/{uid}/asks/{aid}")
+	public Map putAsks(@RequestBody Asks updatedAsk,  HttpServletResponse response) {
+
+		response.setStatus(204);
+		return asksService.updateAsks(updatedAsk);
+		
 	}
 }
