6a124df8153e6f4ada0484b8a0c7d5180a507ec0
Colin Brennan <cbrennan1@hawk.iit.edu>
2022-04-20 19:42:07 -0500

    testing asks first test

---
+++ RestControl/src/main/java/com/example/RestControl/AsksRestController.java
@@ -0,0 +1,39 @@
+package com.example.RestControl;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import javax.servlet.http.HttpServletResponse;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Calendar;
+import java.text.SimpleDateFormat;   
+import java.time.LocalDateTime;   
+import java.time.format.DateTimeFormatter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class AsksRestController {
+
+	@Autowired
+	AsksService asksService;
+	
+	@PostMapping("/bn/api/accounts")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Asks addAccount(@RequestBody Asks newAsks, HttpServletResponse response) {
+		response.setHeader("Location", "/bn/api/accounts/" + newAsks.getUid());
+		return asksService.addAsks(newAsks);
+	}
+}
