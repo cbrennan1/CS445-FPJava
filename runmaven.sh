62b332db2cb1b3ff9f97284103d9649c5ef28c31
Colin Brennan <cbrennan1@hawk.iit.edu>
2022-04-21 19:41:41 -0500

    comments

--- runmaven.sh
+++ runmaven.sh
@@ -2,10 +2,12 @@
 # Automate Maven Set Up for CS445 Final Project
 # Colin Brennan
 
+#Update Maven Enviornment Variables
 sudo chmod +x /etc/profile.d/maven.sh
 source /etc/profile.d/maven.sh
 mvn -version
 
+#Clean Package Run Maven Project
 cd RestControl
 mvn clean
 mvn clean package
